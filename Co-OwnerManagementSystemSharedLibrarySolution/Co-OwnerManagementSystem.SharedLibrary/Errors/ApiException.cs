8190287d6be3e8a872cca541529ec10013f3cb51
LuanHL <hailuanit31032003@gmail.com>
2025-11-11 22:57:50 +0700

    Add ApiException class for standardized error handling
    This commit introduces the `ApiException` class to the shared library, providing a structured way to handle API errors. It includes properties for `StatusCode`, `ErrorCode`, and a custom error message, improving consistency in error management.

---
+++ Co-OwnerManagementSystemSharedLibrarySolution/Co-OwnerManagementSystem.SharedLibrary/Errors/ApiException.cs
@@ -0,0 +1,14 @@
+﻿namespace SU25_Leopard_PE_LyHaiLuan.Business.Models
+{
+    public class ApiException : Exception
+    {
+        public int StatusCode { get; set; }
+        public string ErrorCode { get; set; }
+
+        public ApiException(int statusCode, string errorCode, string message) : base(message)
+        {
+            StatusCode = statusCode;
+            ErrorCode = errorCode;
+        }
+    }
+}
